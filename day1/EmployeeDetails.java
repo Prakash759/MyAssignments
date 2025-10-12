f7c1870a49a81ddd6a0d6980d22c67ad56d16e37
Prakash759 <98072499+Prakash759@users.noreply.github.com>
2025-10-12 11:40:53 +0530

    Add files via upload
    Day 1 Assigments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		String name = "John Doe";
+		short age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		
+		System.out.println("Employee name :"+name+"\nAge:"+age+"\nSalary"+salary+"\nGender:"+gender);
+		System.out.println("AcceptTerms:"+acceptTerms+"\nRating:"+rating+"\nMobile number"+mobileNumber);
+		System.out.println("Pincode:"+pinCode+"\nLucky Number:"+luckyNumber);
+	}
+
+}
